0e18bc28ae3b474c1c1f53115dd5d2c0cba3d706
botho <sbothorel@free.fr>
2026-05-17 14:27:30 +0200

    test

--- servicemanager.cpp
+++ servicemanager.cpp
@@ -42,26 +42,8 @@ bool ServiceManager::writeHistoryToFile(QString fileName, ServiceHistory * histo
 
     // If the STEUERGERAETE_RESET flag is set, append it with an incremented id
     if (this->steuergeraeteReset) {
-        int maxId = 0;
-        if (this->eraseHistory) {
-            maxId = 1; // account for the erase line we wrote as id 1
-        }
-
-        // Normalize line endings then split into lines
-        QString normalized = historyStr;
-        normalized.replace("\r\n", "\n");
-        normalized.replace('\r', '\n');
-        QStringList lines = normalized.split('\n', Qt::SkipEmptyParts);
-        for (const QString &line : lines) {
-            QStringList parts = line.split("|");
-            if (parts.size() > 0) {
-                bool ok = false;
-                int id = parts[0].toInt(&ok);
-                if (ok && id > maxId) maxId = id;
-            }
-        }
-
-        int newId = maxId + 1;
+        // The next id is simply startingOffset + number_of_services + 1
+        int newId = startingOffset + history->services.count() + 1;
         out << QString("%1|g_mmi|STEUERGERAETE_RESET\r\n").arg(newId);
     }
     file.flush();
