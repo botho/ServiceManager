346f041ded9e3388b283be0c242de57a4dbb9f39
botho <sbothorel@free.fr>
2026-05-17 13:05:14 +0200

    fix reset bottom place

--- servicemanager.cpp
+++ servicemanager.cpp
@@ -25,13 +25,44 @@ bool ServiceManager::writeHistoryToFile(QString fileName, ServiceHistory * histo
     }
     QTextStream out(&file);
     int startingOffset = 0;
+    QString eraseLine;
     if (this->eraseHistory) {
-        out << "1|g_mmi|steuern_servicehistory_erase\r\n";
+        eraseLine = "1|g_mmi|steuern_servicehistory_erase\r\n";
         startingOffset = 1;
     }
-    out << history->toString(startingOffset);
+
+    // Generate history string with the appropriate starting offset
+    QString historyStr = history->toString(startingOffset);
+
+    // Write erase line (if any) and the history content
+    if (!eraseLine.isEmpty()) {
+        out << eraseLine;
+    }
+    out << historyStr;
+
+    // If the STEUERGERAETE_RESET flag is set, append it with an incremented id
     if (this->steuergeraeteReset) {
-        out << "1|g_mmi|STEUERGERAETE_RESET\r\n";
+        int maxId = 0;
+        if (this->eraseHistory) {
+            maxId = 1; // account for the erase line we wrote as id 1
+        }
+
+        // Normalize line endings then split into lines
+        QString normalized = historyStr;
+        normalized.replace("\r\n", "\n");
+        normalized.replace('\r', '\n');
+        QStringList lines = normalized.split('\n', Qt::SkipEmptyParts);
+        for (const QString &line : lines) {
+            QStringList parts = line.split("|");
+            if (parts.size() > 0) {
+                bool ok = false;
+                int id = parts[0].toInt(&ok);
+                if (ok && id > maxId) maxId = id;
+            }
+        }
+
+        int newId = maxId + 1;
+        out << QString("%1|g_mmi|STEUERGERAETE_RESET\r\n").arg(newId);
     }
     file.flush();
     file.close();
